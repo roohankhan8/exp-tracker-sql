f1bed8ec984792f7ddeb3e28abdd500dcbf32f45
Roohan Khan <roohank8@gmail.com>
2024-01-13 15:00:02 +0500

    .

---
+++ app/lib/definitions.ts
@@ -0,0 +1,15 @@
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Expense = {
+  id?: string;
+  email: string;
+  category: string;
+  typeOfExp: string;
+  amount: number;
+  date: Date | string;
+};
